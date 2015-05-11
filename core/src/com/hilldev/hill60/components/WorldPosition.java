bbe3b51481db064e4611b0d74a54f6b9b7d9f0eb
Aleksander Kurczyk <akurczyk000@gmail.com>
2015-05-11 15:21:56 +0200

    WorldPosition constructor fix

--- core/src/com/hilldev/hill60/components/WorldPosition.java
+++ core/src/com/hilldev/hill60/components/WorldPosition.java
@@ -1,13 +1,17 @@
 package com.hilldev.hill60.components;
 
 public class WorldPosition extends Component {
-	
+
 	public WorldPosition(float x, float y, boolean boardDependent) {
 		this.x = x;
 		this.y = y;
 		this.boardDependent = boardDependent;
 	}
 	
+	public WorldPosition(float x, float y) {
+		this(x, y, true);
+	}
+
 	public float x;
 	public float y;
 	public boolean boardDependent;

b95322653c026a8c737ec92c7a75ef19da5985dd
Dzejkop <jakubtrad@gmail.com>
2015-05-30 21:54:29 +0200

    ActionManager base

--- core/src/com/hilldev/hill60/objects/ActionManager.java
+++ core/src/com/hilldev/hill60/objects/ActionManager.java
@@ -5,18 +5,29 @@ import com.hilldev.hill60.InputManager;
 import com.hilldev.hill60.components.Behaviour;
 import com.hilldev.hill60.components.BehaviourComponent;
 
+/**
+ * Responds to input from keyboard and mouse
+ * Correlates said input to actions in game world.
+ */
 public class ActionManager extends GameObject {
     public ActionManager(IEngine engine) {
         super(engine);
 
+        // GUISystem guiSystem;
+
         this.addComponent(new BehaviourComponent(new Behaviour() {
 
+            IEngine engine;
             InputManager manager;
             ActionManager parent;
+
+            String currentItem = "Shovel";
 
             @Override
             public void create(BehaviourComponent parentComponent) {
                 parent = (ActionManager)parentComponent.getParent();
+                engine = parent.engine;
+                manager = engine.getInputManager();
             }
 
             @Override

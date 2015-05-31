5099f13fe7218ee86023ac8fb703ceef8dab8894
Dzejkop <jakubtrad@gmail.com>
2015-05-31 16:41:02 +0200

    Some ai shit

--- core/src/com/hilldev/hill60/Scripts/AIScript.java
+++ core/src/com/hilldev/hill60/Scripts/AIScript.java
@@ -1,8 +1,12 @@
 package com.hilldev.hill60.Scripts;
 
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.ai.pfa.indexed.IndexedAStarPathFinder;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.BinaryHeap;
+import com.badlogic.gdx.utils.IntArray;
+import com.hilldev.hill60.Debug;
 import com.hilldev.hill60.IEngine;
 import com.hilldev.hill60.InputManager;
 import com.hilldev.hill60.components.*;
@@ -62,36 +66,34 @@ public class AIScript implements Behaviour {
         // Find player
         player = (Player)engine.findObject("Player");
 
-        Vector2 s = parent.getComponent(BoardPosition.class).getVector();
-        Vector2 e = player.getComponent(BoardPosition.class).getVector();
-
-        currentPath = Path.linearPlot(engine, s, e);
+        finalTarget = new Vector2(5, 5);
 
         if (animationController.getCurrentAnimation() == null)
             animationController.setAnimation(walkSidewaysAnimation);
     }
 
+    Vector2 finalTarget;
+    Vector2 currentTarget;
+    boolean reachedTarget = false;
+
+    int state = 0;
+
     @Override
     public void run() {
+
         animate();
 
-        /*Vector2 n = currentPath.getNext(parent.getComponent(BoardPosition.class));
+        if(state == 0) {
+            // Find target
+        }
 
-        if(n != null) {
-            int x = (int)n.x;
-            int y = (int)n.y;
+        if(state == 1) {
+            // Go to target
+        }
+    }
 
-            goTo(x, y);
-        } else {
-
-            Vector2 s = parent.getComponent(BoardPosition.class).getVector();
-            Vector2 e = player.getComponent(BoardPosition.class).getVector();
-
-            currentPath = Path.linearPlot(engine, s, e);
-        }*/
-
-        BoardPosition playerPos = player.getComponent(BoardPosition.class);
-        goTo(playerPos.x, playerPos.y);
+    public float distance(Vector2 a, Vector2 b) {
+        return (float)Math.sqrt(Math.pow(a.x - b.x ,2) + Math.pow(a.y - b.y ,2));
     }
 
     // In board pos
@@ -112,7 +114,7 @@ public class AIScript implements Behaviour {
         characterScript.goingUp= vy > 0;
         characterScript.goingDown= vy < 0;
 
-        if((vx*vx) + (vy*vy) < 200f) stop();
+        if((vx*vx) + (vy*vy) < 150f) stop();
     }
 
     private void stop() {
@@ -180,6 +182,7 @@ public class AIScript implements Behaviour {
         return f;
     }
 
+
     // For pathfinding
     public static class Path {
 
@@ -188,15 +191,30 @@ public class AIScript implements Behaviour {
             this.nodes = nodes;
         }
 
+
         // Plots a path from start to end (board positions)
-        public static Path plot(IEngine engine, Vector2 startPoint, Vector2 endPoint) {
+        public static Path plot(final IEngine engine, Vector2 startPoint, Vector2 endPoint) {
             List<Vector2> n = new ArrayList<>();
 
-            /*
-            JAKIŚ ALGORYTM ŚCIEŻKI, A* ALBO COŚ
-             */
+            BoardSystem board = engine.getSystem(BoardSystem.class);
 
-            return null;
+            int w = BoardSystem.BOARD_WIDTH;
+            int h = BoardSystem.BOARD_HEIGHT;
+
+            Astar astar = new Astar(w, h) {
+                @Override
+                protected boolean isValid(int x, int y) {
+                    return engine.getSystem(BoardSystem.class).getWallAt(x, y) == null;
+                }
+            };
+
+            IntArray intArray = astar.getPath((int)startPoint.x, (int)startPoint.y, (int)endPoint.x, (int)endPoint.y);
+
+            for(int i = 0 ; i < intArray.size; i+=2) {
+                n.add(new Vector2(intArray.get(i), intArray.get(i+1)));
+            }
+
+            return new Path(n);
         }
 
         // Plots a linear path
@@ -224,22 +242,132 @@ public class AIScript implements Behaviour {
         List<Vector2> nodes;    // Nodes in a path
 
         // Gets the next node in list based on the current position (board positions)
-        public Vector2 getNext(BoardPosition bPos) {
+        public Vector2 getNext() {
+            if(passedByIndex == nodes.size()) return null;
+            return nodes.get(passedByIndex++);
+        }
+    }
 
-            Vector2 b = bPos.getVector();
-            while(passedByIndex < nodes.size()) {
-                Vector2 n = nodes.get(passedByIndex);
+    /* Aquired from gist.github.com/NathanSweet/7587981 */
+    static public class Astar {
+        private final int width, height;
+        private final BinaryHeap<PathNode> open;
+        private final PathNode[] nodes;
+        int runID;
+        private final IntArray path = new IntArray();
+        private int targetX, targetY;
 
-                Vector2 t = new Vector2(n.x - b.x, n.y - b.y);
+        public Astar (int width, int height) {
+            this.width = width;
+            this.height = height;
+            open = new BinaryHeap(width * 4, false);
+            nodes = new PathNode[width * height];
+        }
 
-                float d = t.len();
+        /** Returns x,y pairs that are the path from the target to the start. */
+        public IntArray getPath (int startX, int startY, int targetX, int targetY) {
+            this.targetX = targetX;
+            this.targetY = targetY;
 
-                if(d < 0.5f) passedByIndex++;
-                else break;
+            path.clear();
+            open.clear();
+
+            runID++;
+            if (runID < 0) runID = 1;
+
+            int index = startY * width + startX;
+            PathNode root = nodes[index];
+            if (root == null) {
+                root = new PathNode(0);
+                root.x = startX;
+                root.y = startY;
+                nodes[index] = root;
             }
+            root.parent = null;
+            root.pathCost = 0;
+            open.add(root, 0);
 
-            if(passedByIndex < nodes.size()) return nodes.get(passedByIndex);
-            return null;
+            int lastColumn = width - 1, lastRow = height - 1;
+            int i = 0;
+            while (open.size > 0) {
+                PathNode node = open.pop();
+                if (node.x == targetX && node.y == targetY) {
+                    while (node != root) {
+                        path.add(node.x);
+                        path.add(node.y);
+                        node = node.parent;
+                    }
+                    break;
+                }
+                node.closedID = runID;
+                int x = node.x;
+                int y = node.y;
+                if (x < lastColumn) {
+                    addNode(node, x + 1, y, 10);
+                    if (y < lastRow) addNode(node, x + 1, y + 1, 14); // Diagonals cost more, roughly equivalent to sqrt(2).
+                    if (y > 0) addNode(node, x + 1, y - 1, 14);
+                }
+                if (x > 0) {
+                    addNode(node, x - 1, y, 10);
+                    if (y < lastRow) addNode(node, x - 1, y + 1, 14);
+                    if (y > 0) addNode(node, x - 1, y - 1, 14);
+                }
+                if (y < lastRow) addNode(node, x, y + 1, 10);
+                if (y > 0) addNode(node, x, y - 1, 10);
+                i++;
+            }
+            return path;
+        }
+
+        private void addNode (PathNode parent, int x, int y, int cost) {
+            if (!isValid(x, y)) return;
+
+            int pathCost = parent.pathCost + cost;
+            float score = pathCost + Math.abs(x - targetX) + Math.abs(y - targetY);
+
+            int index = y * width + x;
+            PathNode node = nodes[index];
+            if (node != null && node.runID == runID) { // Node already encountered for this run.
+                if (node.closedID != runID && pathCost < node.pathCost) { // Node isn't closed and new cost is lower.
+                    // Update the existing node.
+                    open.setValue(node, score);
+                    node.parent = parent;
+                    node.pathCost = pathCost;
+                }
+            } else {
+                // Use node from the cache or create a new one.
+                if (node == null) {
+                    node = new PathNode(0);
+                    node.x = x;
+                    node.y = y;
+                    nodes[index] = node;
+                }
+                open.add(node, score);
+                node.runID = runID;
+                node.parent = parent;
+                node.pathCost = pathCost;
+            }
+        }
+
+        protected boolean isValid (int x, int y) {
+            return true;
+        }
+
+        public int getWidth () {
+            return width;
+        }
+
+        public int getHeight () {
+            return height;
+        }
+
+        static private class PathNode extends BinaryHeap.Node {
+            int runID, closedID, x, y, pathCost;
+            PathNode parent;
+
+            public PathNode (float value) {
+                super(value);
+            }
         }
     }
 }
